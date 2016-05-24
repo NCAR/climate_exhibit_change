0d2bfc99a97116c082cae6289832710bd18fef56
Sharon Clark <sclark@ucar.edu>
2016-05-24 11:38:39 -0600

    IIFE and named functions

--- js/app.js
+++ js/app.js
@@ -1,43 +1,42 @@
-angular.module("edu.ucar.scied.climatechange", [
-    "edu.ucar.scied.controllers", 
+(function () {
+    'use strict';
+    angular.module("edu.ucar.scied.climatechange", [
+    "edu.ucar.scied.controllers",
     "edu.ucar.scied.controllers.videos",
     "edu.ucar.scied.controllers.climatechange",
     "edu.ucar.scied.controllers.climatechange.diy_earth",
     "edu.ucar.scied.services",
     "edu.ucar.scied.directives",
     "edu.ucar.scied.filters",
-    "ngRoute",  
-    "ngMaterial",    
+    "ngRoute",
+    "ngMaterial",
     "com.2fdevs.videogular",
     "com.2fdevs.videogular.plugins.controls",
     "com.2fdevs.videogular.plugins.overlayplay",
     "com.2fdevs.videogular.plugins.poster",
-    "angulartics", 
+    "angulartics",
     "angulartics.google.analytics"
-]).
-config(["$routeProvider", function($routeProvider) {
-  $routeProvider.
-	when("/", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+    ]).
+    config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.
+        when("/", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "homeCtrl",
-        }
-    ).
-   when("/videos", 
-         {
-            templateUrl: "/core/templates/menu_grid.html", 
+        }).
+        when("/videos", {
+            templateUrl: "/core/templates/menu_grid.html",
             controller: "videosCtrl",
-        }
-    ).
-    when("/videos/:videoId", 
-         {
-            templateUrl: "/core/templates/video_player.html", 
+        }).
+        when("/videos/:videoId", {
+            templateUrl: "/core/templates/video_player.html",
             controller: "playerCtrl"
-        }
-    ).
-    when("/apps/diy-earth", {
-        templateUrl: "templates/diy_earth.html",
-        controller: "diyEarthCtrl"
-    }).
-	otherwise({redirectTo: '/'});
-}]);
+        }).
+        when("/apps/diy-earth", {
+            templateUrl: "templates/diy_earth.html",
+            controller: "diyEarthCtrl"
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }]);
+})();
