686abc6685b45eda28fb80613f5cd8505744ec4e
Sharon Clark <sclark@ucar.edu>
2016-06-06 13:32:18 -0600

    put templates with js code

--- js/app.js
+++ js/app.js
@@ -23,19 +23,19 @@
     config(["$routeProvider", function ($routeProvider) {
         $routeProvider.
         when("/", {
-            templateUrl: "/core/templates/menu_grid.html",
+            templateUrl: "/core/js/menulist/menu_grid.html",
             controller: "homeCtrl",
         }).
         when("/videos", {
-            templateUrl: "/core/templates/menu_grid.html",
+            templateUrl: "/core/js/menulist/menu_grid.html",
             controller: "videosCtrl",
         }).
         when("/videos/:videoId", {
-            templateUrl: "/core/templates/video_player.html",
+            templateUrl: "/core/js/videos/video_player.html",
             controller: "playerCtrl"
         }).
         when("/apps/diy-earth", {
-            templateUrl: "templates/diy_earth.html",
+            templateUrl: "js/diy_earth/diy_earth.html",
             controller: "diyEarthCtrl"
         }).
         otherwise({
